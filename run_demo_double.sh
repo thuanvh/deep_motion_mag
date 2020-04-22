aa7dd55f1e447ffba3452a8c8b713fbed7bb2e26
Thuan <thuanvh@gmail.com>
2020-04-22 23:50:05 +0700

    Update video test script

--- run_demo_double.sh
+++ run_demo_double.sh
@@ -1,5 +1,12 @@
-python3 live_demo_multiples.py -video /home/thuan/Downloads/webcams4.webm -rect 0,120,320,240 &
+#python3 live_demo_multiples.py -video /home/thuan/Downloads/webcams4.webm -rect 0,120,320,240 &
 
-python3 live_demo_multiples.py -video /home/thuan/Downloads/webcams4.webm -rect 320,120,320,240 -mag 5
+#python3 live_demo_multiples.py -video /home/thuan/Downloads/webcams4.webm -rect 320,120,320,240 -mag 5
+
 #python3 live_demo.py -video /home/thuan/Downloads/webcams2.webm
+
 #python3 live_demo.py -video /home/thuan/Downloads/webcams1.webm
+
+python3 live_demo_multiples.py -video /home/thuan/Downloads/webcams6.webm -rect 0,120,320,240 &
+
+python3 live_demo_multiples.py -video /home/thuan/Downloads/webcams6.webm -rect 320,120,320,240
+
